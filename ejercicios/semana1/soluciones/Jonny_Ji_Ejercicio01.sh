07b3cf0ae22668d7defc7968a9aebebc992e4434
Jonny Ji <157997680+JonnyFJi@users.noreply.github.com>
2025-07-04 03:35:16 -0400

    Update Jonny_Ji_Ejercicio01.sh

--- ejercicios/semana1/soluciones/Jonny_Ji_Ejercicio01.sh
+++ ejercicios/semana1/soluciones/Jonny_Ji_Ejercicio01.sh
@@ -209,41 +209,90 @@ else
 fi
 
 # Terminado
+[+] ¡Su nodo de Bitcoin ahora está instalado y en funcionamiento!
+
+echo "# ¡Bitcoin core está instalado y configurado con exito!"
+echo -e "\n# Script de ejecución para el ejercicio 1\n"
+echo -e "¡Bitcoin core está en ejecución!\n"
+echo -e "Verificar el estado de Bitcoin Core\n"
+
+# Escribir el comando getblockchaininfo para ver los datos de la red y la altura de la cadena, debe mostrar la red 'regtest', bloque '0'
+bitcoin-cli getblockchaininfo |jq
+
+echo -e "\nAltura de Bloque"
+bitcoin-cli getblockcount
+
+# Generar dos nuevas carteras
+echo -e "\nCrear una billetera llamada Miner"
+bitcoin-cli -named createwallet wallet_name="Miner" descriptors=false
+
+echo -e "\nCrear una billetera llamada Trader"
+bitcoin-cli -named createwallet wallet_name="Trader" descriptors=false
+
+echo -e "\nVer los detalles de la billetera Miner"
+bitcoin-cli -rpcwallet=Miner getwalletinfo |jq
+
+echo -e "\nVer los detalles de la billetera Trader"
+bitcoin-cli -rpcwallet=Trader getwalletinfo |jq
+
+# Generar una dirección
+echo -e "\nCrear una nueva dirección en la wallet 'Miner' para recibir "
+bitcoin-cli -rpcwallet=Miner getnewaddress "Recompensa de Mineria"
+
+echo -e "\n# Copiar la dirección 'Recompensa de Minería' "
+echo -e "\nIngresa la dirección que acabas de copiar"
+read Recompensa
+echo -e "\n# Minar a la nueva dirección"
+bitcoin-cli -rpcwallet=Miner generatetoaddress 101 $Recompensa
+
+echo -e "\nVerificar el estado de la cadena"
+echo "Altura de Bloque"
+bitcoin-cli getblockcount
+
+echo -e "\nVer los detalles de la billetera Miner"
+bitcoin-cli -rpcwallet=Miner getwalletinfo |jq
+
+echo -e "\nVer el balance de la billetera Miner"
+bitcoin-cli -rpcwallet=Miner getbalance
+
+echo -e "\nPodemos identificar los UTXO disponibles"
+echo "# Podemos ver detalles como 'txid', 'vout', 'address', 'label', 'scripttpubkey', 'amount', 'confirmations'"
+bitcoin-cli -rpcwallet=Miner listunspent |jq
+
+echo -e "\n¿Cuántos bloques se necesitan para obtener un saldo positivo? "
+echo "Se necesitan 100 bloques confirmados para que el saldo de la billetera se muestre positivo"
+echo -e "\n¿Por qué el saldo de la billetera se comporta así? "
+echo "El saldo de cada bloque minado necesita 100 bloques para estar disponible para ser gastado, se le denomina a madurar el saldo o madurar la tx coinbase."
+
+echo -e "\nBalance de la billetera Miner"
+bitcoin-cli -rpcwallet=Miner getbalance
+
+# Hacer una transacción
+echo -e "\n# Crear una dirección receptora con la etiqueta 'Recibido' en la billetera Trader."
+bitcoin-cli -rpcwallet=Trader getnewaddress "Recibido"
+
+echo -e "\n# Copiar la dirección 'Recibido' "
+echo -e "\nIngresa la dirección que acabas de copiar"
+read Recibido
+echo -e "\n# Enviar una transacción que pague 20 BTC desde la billetera Miner a la billetera Trader." 
+echo -e "\nTxid= "
+bitcoin-cli -rpcwallet=Miner sendtoaddress $Recibido 20.000
+
+echo -e "\n# Copiar el TxId de la transacción que se acaba de enviar"
+echo -e "\nObtener la transacción no confirmada desde el 'mempool' del nodo y mostrar el resultado"
+echo -e "\nIngresa el Txid que acabas de copiar"
+read TxId
+bitcoin-cli getmempoolentry $TxId |jq
+
+echo -e "\nMinar un bloque para que la transacción se confirme"
+bitcoin-cli -rpcwallet=Miner -generate 1
+
+echo -e "\n# Obtener los detalles de la transacción y mostrarlos en la terminal:"
+bitcoin-cli getrawtransaction $TxId 1 |jq
+
 cat <<"EOF"
 
-[+] ¡Bitcoin Core está instalado, configurado y el servicio habilitado con éxito!
-[+] ¡Hay que reiniciar el equipo para que algunos cambios tengan efecto!
-[+] ¡Este es un ambiente de pruebas para ejecutar comandos de Bitcoin Core!
-[+] ¡Se instalo la versión v25.0, para crear billeteras Legacy y poder extraer la privkey!
-[+]	"1. Para comenzar crea dos carteras nuevas usando los comandos para billeteras legacy"
-[+]	    "$ bitcoin-cli -named createwallet wallet_name="Miner" descriptors=false "
-[+]	    "$ bitcoin-cli -named createwallet wallet_name="Trader" descriptors=false "
-[+]	"2. Genera una dirección en la billetera Miner con etiqueta "Recompensa de Minería" "
-[+]	    "$ bitcoin-cli -rpcwallet=Miner getnewaddress "Recompensa de Mineria"
-[+]	"3. Extraer nuevos bloques depositando la recompensa a esta dirección"
-[+]	"Para obtener un saldo de billetera positivo se utiliza el comando 'generatetoaddress' "
-[+]	"El siguiente comando genera un bloque (o varios) y lo envia a la direccion"
-[+]	    "$ bitcoin-cli generatetoaddress 1 "direccion-recompensa" "
-[+]	"El siguiente comando es para visualizar el saldo en la cartera"
-[+]	    "$ bitcoin-cli -rpcwallet=Miner getbalance "
-[+] "Podemos identificar los UTXO que tenemos disponibles con el siguiente comando"
-[+]     "$ bitcoin-cli -rpcwallet=Miner listunspent "
-[+] "En pantalla vemos detalles como "txid", "vout", "address", "label", "scripttpubkey", "amount", "confirmations" "
-[+] "4. Crear una dirección receptora con la etiqueta "Recibido" desde la billetera Trader "
-[+] "5. Enviar una transacción que pague 20 BTC desde la billetera Miner a la billetera Trader "
-[+]     "$ bitcoin-cli -rpcwallet=Miner sendtoaddress <<address_Recibido>> 20.000 "
-[+] "6. Obtener la transacción no confirmada desde el "mempool" del nodo y mostrar el resultado"
-[+]     "$ bitcoin-cli getmempoolentry <<Txid>> "
-[+] "7. Generar una nueva dirección para generar el nuevo bloque "
-[+]     "$ bitcoin-cli -rpcwallet=Miner getnewaddress "
-[+] "8. Confirmar la transacción creando 1 bloque adicional "
-[+]     "$ bitcoin-cli generatetoaddress 1 <<new_address_Miner>> "
-[+] "9. Obtener los siguientes detalles de la transacción y mostrarlos en la terminal: "
-[+]     "$ bitcoin-cli -rpcwallet=Miner gettransaction <<TxId_transacción_20BTC>> "
-[+] "10. Verificando el balance de la wallet trader "
-[+]     "$ bitcoin-cli -rpcwallet=Trader listunspent "
-[+]	"Si tienes dudas de cualquier comando ejecuta '$ bitcoin-cli help' "
-[+]	¡Puedes copiar estas instrucciones para que te sirvan de guía!
+[+]	¡Fin del ejercicio 1!"
 
         ⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀
         ⣿⣿⣿⣿⠟⠿⠿⡿⠀⢰⣿⠁⢈⣿⣿⣿⣿⣿⠀⠀
@@ -257,7 +306,6 @@ cat <<"EOF"
         ⣿⣿⣿⣿⣄⣰⣿⠁⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
         ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀v25.0⠀⠀⠀⠀⠀⠀⠀
 
-[+] ¡Recuerda reiniciar el equipo para que algunos cambios tengan efecto!
-[+] ¡Su nodo de Bitcoin ahora está instalado y en funcionamiento!
+[+] ¡Librería de Satoshi!
+[+]	¡Bitcoin from Command Line!
 EOF
-
